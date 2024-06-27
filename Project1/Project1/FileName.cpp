d35658b3547418a767ae6df26eb81a5cef0f7692
Slipchuk Max <maxslipchuk11@gmail.com>
2024-06-27 17:55:09 +0300

    Add function perevirka

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -4,7 +4,25 @@
 #include <ctime>
 #define _CRT_SECURE_NO_WARNINGS
 using namespace std;
+bool Perevirka(const char* str) {
+    int a = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '(') {
+            a++;
+        }
+        else if (str[i] == ')') {
+            a--;
+        }
+    }
+    if (a==0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 int main() {
     char str[50];
     cin.getline(str, 50);
+    
 }
